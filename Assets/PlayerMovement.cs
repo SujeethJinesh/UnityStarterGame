7ca3bfd7088ffa9424a556c6ad5693d60589d4ad
SujeethJinesh <sujeethjinesh@gmail.com>
2018-04-15 16:31:13 -0700

    Add Force to Object

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public Rigidbody rigidBody;
+
+	// Update is called once per frame
+	void FixedUpdate () //whenever you do physics, do it here and NEVER in Update
+	{
+        //Time.deltaTime = evens out advantage of gaming on a better comp
+        rigidBody.AddForce (0, 0, 2000 * Time.deltaTime);
+	}
+}
